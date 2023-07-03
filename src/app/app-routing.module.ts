574d49f5327b8af5087b74eea4007fcfe82b8cd5
unknown <hedirbenmoussa71@gmail.com>
2023-07-03 17:32:08 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/shared/home/home.component';
+import { NotfoundComponent } from './components/shared/notfound/notfound.component';
+import { AboutUsComponent } from './components/shared/about-us/about-us.component';
+import { SigninComponent } from './signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //redirection 
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {path:'home', component:HomeComponent},
+  {path:'aboutus', component:AboutUsComponent},
+  {path:'signin', component:SigninComponent},
+
+
+
+
+  {path:'**', component:NotfoundComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './components/shared/home/home.component';
+import { NotfoundComponent } from './components/shared/notfound/notfound.component';
+import { AboutUsComponent } from './components/shared/about-us/about-us.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    NotfoundComponent,
+    AboutUsComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
