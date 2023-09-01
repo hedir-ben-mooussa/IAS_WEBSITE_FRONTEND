0a3347a4e4b3b6b59e525001a20087e2e1afd3fc
hedir-ben-mooussa <hedirbenmoussa71@gmail.com>
2023-09-01 15:36:34 +0100

    🐛 fixed bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,35 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/shared/header/header.component';
-import { FooterComponent } from './components/shared/footer/footer.component';
-import { HomeComponent } from './components/shared/home/home.component';
-import { NotfoundComponent } from './components/shared/notfound/notfound.component';
-import { AboutUsComponent } from './components/shared/about-us/about-us.component';
 import { SigninComponent } from './components/auth/signin/signin.component';
-import { JoinusComponent } from './components/joinus/joinus.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ActivitiesComponent } from './components/activities/activities.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
-import { ContactComponent } from './components/contact/contact.component';
-import { EventListComponent } from './components/event-list/event-list.component';
-import { UserListComponent } from './components/user-list/user-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
-    NotfoundComponent,
-    AboutUsComponent,
+    AppComponent, 
     SigninComponent,
-    JoinusComponent,
-    ActivitiesComponent,
-    ContactComponent,
-    EventListComponent,
-    UserListComponent,
   ],
   imports: [
     BrowserModule,
