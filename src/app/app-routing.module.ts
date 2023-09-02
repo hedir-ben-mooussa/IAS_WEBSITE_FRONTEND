49ba25404d4aab2086ad9e253d7de24015d4724d
hedir-ben-mooussa <hedirbenmoussa71@gmail.com>
2023-09-02 01:46:54 +0100

    ✅ update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './components/auth/signin/signin.component';
 import { NotfoundComponent } from './components/frontoffice/shared/notfound/notfound.component';
+import { JoinusComponent } from './components/joinus/joinus.component';
 
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./components/frontoffice/frontoffice.module').then(m => m.FrontofficeModule) },
   { path: 'signin', component: SigninComponent },
-  { path: 'backoffice', loadChildren: () => import('./components/backoffice/backoffice.module').then(m => m.BackofficeModule) },
+  { path: 'joinus', component: JoinusComponent },
+  { path: 'adminpanel', loadChildren: () => import('./components/backoffice/backoffice.module').then(m => m.BackofficeModule) },
   { path: '**', component: NotfoundComponent },
 ];
 
